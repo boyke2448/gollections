3921f1bc64cbe2ea54a5f88b593ec1cc6908347e
boyke2448 <boyke2448@users.noreply.github.com>
2022-04-21 00:05:31 +0200

    graph improvements

---
+++ graph/edge.go
@@ -0,0 +1,6 @@
+package graph
+
+type Edge[T comparable] struct {
+	VertexA T
+	VertexB T
+}
---
+++ graph/graph-operations.go
@@ -0,0 +1,26 @@
+package graph
+
+func (g *graph[T]) GetEdges() []Edge[T] {
+	return g.edges
+}
+
+func (g *graph[T]) FindAdjecentVertices(vertex T) []T {
+	adjecentVertices := make([]T, 0)
+	for _, edge := range g.GetEdges() {
+		if edge.VertexA == vertex {
+			adjecentVertices = append(adjecentVertices, edge.VertexB)
+		} else if edge.VertexB == vertex {
+			adjecentVertices = append(adjecentVertices, edge.VertexA)
+		}
+	}
+	return adjecentVertices
+}
+
+func (g *graph[T]) IsConnected(vertexA, vertexB T) bool {
+	for _, edge := range g.GetEdges() {
+		if (edge.VertexA == vertexA && edge.VertexB == vertexB) || (edge.VertexA == vertexB && edge.VertexB == vertexA) {
+			return true
+		}
+	}
+	return false
+}
